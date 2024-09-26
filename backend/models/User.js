a529e39325eaacc54e8fe056fc29cb03ba4ce67e
lakhabhai ghoghol <ghoghollaxman@gmail.com>
2024-09-26 21:03:43 +0530

    fixed file name

---
+++ backend/models/User.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        min: 4,
+        max: 30,
+        unique: true
+    },
+    email:{
+        type: String,
+        required: true,
+        min: 6,
+        max: 30,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true,
+        min: 6
+    },
+    profilePic:{
+        type: String,
+        default: ""
+    },
+    CoverPic:{
+        type: String,
+        default: ""
+    },
+    followers:{
+        type: Array,
+        default: []
+    },
+    followings:{
+        type: Array,
+        default: []
+    },
+    isAdmin:{
+        type: Boolean,
+        default: false
+    },
+    desc:{
+        type: String,
+        max: 100
+    },
+    city:{
+        type: String,
+        max: 50
+    },
+    from:{
+        type: String,
+        max: 50
+    },
+    relationship:{
+        type: String,
+        max: 20
+    },
+
+},
+{timestamps: true}
+);
+
+module.exports = mongoose.model("User", UserSchema)
